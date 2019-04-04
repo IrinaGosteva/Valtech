ac83cc3a019efa69fa0d09d13d9de6a6fa276a56
Irina Gosteva <IrinaGosteva@users.noreply.github.com>
2019-04-04 12:02:16 +0300

    after jeneral notes

--- js/init.js
+++ js/init.js
@@ -23,7 +23,7 @@ function onScroll() {
             'top' : '0',
             'z-index' : '999',
             'transition' : 'top .3s ease-out',
-            'box-shadow': '0px 3px 10px 0px rgba(0,0,0,0.1)'
+            'box-shadow': '0px 3px 5px 0px rgba(0,0,0,0.1)'
         });
     }
 
@@ -42,16 +42,39 @@ function onScroll() {
 $(window).on('scroll', onScroll);
 
 // smoth scroll
-$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
+// $('a[href^="#"]').on('click', function(event) {
+//     var target = $(this.getAttribute('href'));
+//     if( target.length) {
+//         event.preventDefault();
+//         $('html, body').stop().animate({
+//             scrollTop: target.offset().top
+//         }, 1000);
+//     }
+// });
+
+
+$('.scroll').on('click',function(e) {
+    e.preventDefault();
+    var offset = 0;
+    var target = this.hash;
+    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+    $('html, body').stop().animate({
+        'scrollTop': $(target).offset().top - offset
+    }, 1000, 'swing', function() {
+        // window.location.hash = target;
+    });
 });
+
+
 })(jQuery);
+
+// $('.scroll').on('click', function(e) {
+//     e.preventDefault();
+//     var target = $(this).attr('href');
+//     var offset = 0;
+//     if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+//     $.scrollTo(target, 300, { offset: -offset });
+// }1000);
 
     $(function(){
         $('.header-nav').slicknav({
